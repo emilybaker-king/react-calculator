d4aa4bab1d22b50633e371e143427c19b63182ba
Emily Baker-King <ebakerking@gmail.com>
2019-11-06 13:40:36 -0600

    added in calculator component and styled the app and calculator components

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import './Calculator.css';
+
+/* main calculator component. Contains our other calculator components */
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="Calculator">
+                
+            </div>
+        );
+    }
+}
+
+export default Calculator;
