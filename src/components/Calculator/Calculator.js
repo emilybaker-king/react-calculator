974a581bb208051c1f31505be92bd55b250df73d
Emily Baker-King <ebakerking@gmail.com>
2019-11-08 14:15:43 -0600

    finished up the calculator functions and made it look pretty

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -17,15 +17,54 @@ class Calculator extends Component {
         }
     }
 
+    clear() {
+        this.setState({ equation: '', result: 0 });
+    }
+
+    /* function that is called when any of the ketpad buttons are pressed */
     onButtonPress = (event) => {
-        console.log("onButtonPressed called");
+        let equation = this.state.equation;
+        const pressedButtonValue = event.target.innerHTML;
+
+        console.log(pressedButtonValue);
+
+        if (pressedButtonValue === 'C') {
+            this.clear();
+            return;
+        } else if ((pressedButtonValue >= '0' && pressedButtonValue <= '9') || pressedButtonValue === '.') {
+            /* if the user has pressed a button with a value between 0 and 9 or a period, add that value to the equation */
+            equation += pressedButtonValue;
+        } else if (['+', '-', '*', '/', '%'].indexOf(pressedButtonValue) !== -1) {
+            /* if the user has pressed a button with the value of an operator, add the operator to the equation with a space on either side */
+            equation += ' ' + pressedButtonValue + ' ';
+        } else if (pressedButtonValue === "=") {
+            /* if the user hits the enter button, calculate stuff */
+            /* Try catch blocks - try to run the code block. if it fails and throws an error, deal with it in the catch block */
+            try {
+                const evalResult = eval(equation);
+                const result = Number.isInteger(evalResult) ? evalResult : evalResult.toFixed(2);
+                this.setState({ result });
+            } catch (error) {
+                /* if an error occurs, print it to the console and alert the user */
+                console.log(error);
+                alert('An error occured. Check to make sure your equation is correct.');
+            }
+
+        } else {
+            /* if the user has pressed the back arrow, remove the last character from the equation string */
+            equation = equation.trim();
+            equation = equation.substr(0, equation.length - 1);
+        }
+
+        /* update our state with the new version of the equation */
+        this.setState({ equation });
     }
 
     render() {
         return (
             <div className="Calculator">
                 <Screen equation={this.state.equation} result={this.state.result} />
-                <Keypad onButtonPress={this.onButtonPress()} />
+                <Keypad onButtonPress={this.onButtonPress} />
             </div>
         );
     }
--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -12,38 +12,38 @@ class Keypad extends Component {
                 <KeypadRow>
                     <Button onButtonPress={this.props.onButtonPress} type='primary'>C</Button>
                     <Button onButtonPress={this.props.onButtonPress} type='operator'>&larr;</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>%</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>/</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>%</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>/</Button>
                 </KeypadRow>
 
                 {/* Row 2 */}
                 <KeypadRow>
-                    <Button onButtonPress={this.props.onButtonPress}>7</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>8</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>9</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>*</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>7</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>8</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>9</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>*</Button>
                 </KeypadRow>
 
                 {/* Row 3 */}
                 <KeypadRow>
-                    <Button onButtonPress={this.props.onButtonPress}>4</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>5</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>6</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>-</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>4</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>5</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>6</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>-</Button>
                 </KeypadRow>
 
                 {/* Row 4 */}
                 <KeypadRow>
-                    <Button onButtonPress={this.props.onButtonPress}>1</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>2</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>3</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>+</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>1</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>2</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>3</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>+</Button>
                 </KeypadRow>
                 
                 {/* Row 5 */}
                 <KeypadRow>
-                    <Button onButtonPress={this.props.onButtonPress}>0</Button>
-                    <Button onButtonPress={this.props.onButtonPress}>.</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='nums'>0</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator-period'>.</Button>
                     <Button onButtonPress={this.props.onButtonPress} type='equalButton'>=</Button>
                 </KeypadRow>
             </div>
