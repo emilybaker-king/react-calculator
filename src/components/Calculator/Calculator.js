ec7a271c35b6a989e2ed04f77154b848d704bf49
Emily Baker-King <ebakerking@gmail.com>
2019-11-08 10:43:05 -0600

    Added in the Button component + styling. Added state for result and equation and button handler.

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -6,11 +6,26 @@ import Keypad from '../Keypad/Keypad';
 
 /* main calculator component. Contains our other calculator components */
 class Calculator extends Component {
+
+    /* constructor function to set our initial state */
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            equation: '',
+            result: 0
+        }
+    }
+
+    onButtonPress = (event) => {
+        console.log("onButtonPressed called");
+    }
+
     render() {
         return (
             <div className="Calculator">
-                <Screen />
-                <Keypad />
+                <Screen equation={this.state.equation} result={this.state.result} />
+                <Keypad onButtonPress={this.onButtonPress()} />
             </div>
         );
     }
